60727c1569318900aed69b7df7d2584b33297105
Terry Weymouth <weymouth@umich.edu>
2019-07-04 09:50:31 -0400

    initial version of nearest road

--- src/probe_by_distance.py
+++ src/probe_by_distance.py
@@ -3,7 +3,8 @@ from psycopg2 import sql
 
 connection = None
 cursor = None
-max_d = 0;
+max_d = 0
+
 
 def make_connection():
     global connection, cursor
@@ -15,37 +16,82 @@ def make_connection():
     cursor = connection.cursor()
 
 
-def probe(id):
-    global max_d
+def get_id_limits(track_id):
+    query_str = "select min(id), max(id) from bicycle_data where track_id={};"
+    query_str = query_str.format(track_id)
+    query = sql.SQL(query_str)
+    cursor.execute(query)
+    results = cursor.fetchone()
+    return results
+
+
+def probe(track_id, point_id):
+    global cursor, max_d
     query_str = "select track.id, osm.osm_id, osm.name, osm.highway, "\
-            + "ST_Distance(ST_Transform(osm.way,4326),track.long_lat_original) "\
-            + "from bicycle_data as track, planet_osm_line as osm "\
-            + "where track.id={} and "\
-            + "ST_Distance(ST_Transform(osm.way,4326),track.long_lat_original) < 30.0 "\
-            + "order by st_distance limit 1;"
-    query_str = query_str.format(id)
+        + "ST_Distance(ST_Transform(osm.way,4326),track.long_lat_original) "\
+        + "from bicycle_data as track, planet_osm_line as osm "\
+        + "where track.track_id={} and track.id={} and "\
+        + "ST_Distance(ST_Transform(osm.way,4326),track.long_lat_original) < 70.0 " \
+        + "and highway is not null " \
+        + "and not (highway in ('footway', 'service', 'tertiary_link', 'motorway'))" \
+        + "order by st_distance limit 1;"
+    query_str = query_str.format(track_id, point_id)
     query = sql.SQL(query_str)
     cursor.execute(query)
     results = cursor.fetchone()
     if results:
-        print(results)
         d = results[4]
         if d > max_d:
             max_d = d
     else:
         print("Missing id =", id)
+    return results
+
+
+def update_records(results_list):
+    global cursor, connection
+    for item in results_list:
+        print(item)
+        point_id = item[0]
+        way_id = item[1]
+        distance = item[4]
+        query = sql.SQL("UPDATE bicycle_data "
+                        + "SET nearest_road_id=%s, "
+                        + "nearest_road_distance=%s "
+                        + "where id={};"
+                        .format(point_id))
+        cursor.execute(query, (way_id, distance))
+    connection.commit()
+
+
+def record_max_distance(track_id, d):
+    global cursor, connection
+    query = sql.SQL("UPDATE bicycle_track "
+                    + "SET max_distance=%s "
+                    + "where track_id={};"
+                    .format(track_id))
+    cursor.execute(query, (d,))
+    connection.commit()
+
 
 def main():
     global connection, cursor, max_d
+    track_index = 1
     try:
         make_connection()
-        for id in range(1, 2956):
-            probe(id)
-        print(max_d)
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
+        limits = get_id_limits(track_index)
+        results_list = []
+        for point_id in range(limits[0], limits[1] + 1):
+            results = probe(track_index, point_id)
+            if results:
+                results_list.append(results)
+        print(results_list)
+        update_records(results_list)
+        record_max_distance(track_index, max_d)
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
     finally:
-        #closing database connection.
+        # closing database connection.
         if connection:
             cursor.close()
             connection.close()
