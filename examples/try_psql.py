a4a8d97f826a7cc96a169f8eb2039f85352ac9d6
Terry Weymouth <weymouth@umich.edu>
2023-12-02 18:43:31 -0500

    update minor problems

--- examples/try_psql.py
+++ examples/try_psql.py
@@ -1,18 +1,19 @@
 import psycopg2
+connection = None
 try:
-    connection = psycopg2.connect(user = "weymouth",
-                                  password = "",
-                                  host = "127.0.0.1",
-                                  port = "5432",
-                                  database = "Detroit")
+    connection = psycopg2.connect(user="weymouth",
+                                  password="",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="Detroit")
     cursor = connection.cursor()
     # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
+    print ( connection.get_dsn_parameters(), "\n")
     # Print PostgreSQL version
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
-except (Exception, psycopg2.Error) as error :
+    print("You are connected to - ", record, "\n")
+except (Exception, psycopg2.Error) as error:
     print ("Error while connecting to PostgreSQL", error)
 finally:
     #closing database connection.
