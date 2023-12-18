063652f5de931425b9e2a24db5c6541bc6a1472e
Terry Weymouth <weymouth@umich.edu>
2023-12-17 20:16:09 -0500

    Working example using map-gl

--- examples/mapgl_example.py
+++ examples/mapgl_example.py
@@ -31,13 +31,74 @@ def connect_and_query():
     return record
 
 
-def add_geom(out_file, geom_list):
-    for i in range(0, len(geom_list)):
-        geom = geom_list[i][0]
-        name = "geom{}".format(i)
+"""
+        map.on('style.load', () => {
+            var geom0 = {"type":"LineString",
+                    "coordinates":[[-83.1590104,42.499099601],[-83.1590112,42.499171101],[-83.1590466,42.500186201],[-83.1590488,42.500252101],[-83.1590516,42.500322401],[-83.1590853,42.501180001],[-83.1590857,42.501244901],[-83.1590895,42.501308701],[-83.1591236,42.502278901]]
+            };
+            map.addSource('route', {
+                type: 'geojson',
+                data: {
+                    'type': 'Feature',
+                    'properties': {},
+                    'geometry': geom0 }
+            });
+            map.addLayer({
+                id: 'route',
+                type: 'line',
+                source: 'route',
+                paint: {
+                    'line-color': '#888',
+                    'line-width': 3
+                }
+            });
+        });
+"""
 
-        out_file.write("\tvar {} = {};\n".format(name, geom))
-        out_file.write("\tL.geoJSON({}).addTo(mymap);\n".format(name))
+
+def add_geom(out_file, geom_list):
+    # loader header
+    out_file.write("map.on('style.load', () => {\n")
+    # loader content
+    for i in range(0, len(geom_list)):
+        print("loop", i)
+        geom_item = geom_list[i][0]
+        geom_name = "geom{}".format(i)
+        map_item_name = "track{}".format(i)
+        doc_string = f"""
+            var {geom_name} = {geom_item};
+            map.addSource('{map_item_name}', {{
+                type: 'geojson',
+                data: {{
+                    'type': 'Feature',
+                    'properties': {{}},
+                    'geometry': {geom_name} }}
+            }});
+            map.addLayer({{
+                id: '{map_item_name}',
+                type: 'line',
+                source: '{map_item_name}',
+                paint: {{
+                    'line-color': '#888',
+                    'line-width': 3
+                }}
+            }});
+        """
+        out_file.write(doc_string)
+    # loader footer
+    out_file.write("});\n")
+
+
+def add_map_base(out_file):
+    out_file.write(
+        """\t\tconst map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [-83.1592,42.4939],
+            zoom: 16.5,
+        });
+        """
+    )
 
 
 def copy_path_content(in_path, out_file):
@@ -52,10 +113,12 @@ def add_access_token(out_file):
 
 def main():
     road_geom = connect_and_query()
+    print(len(road_geom))
     output = "mapgl_example.html"
     with open(output, "w") as out_file:
         copy_path_content(header_filepath, out_file)
         add_access_token(out_file)
+        add_map_base(out_file)
         add_geom(out_file, road_geom)
         copy_path_content(footer_filepath, out_file)
 
