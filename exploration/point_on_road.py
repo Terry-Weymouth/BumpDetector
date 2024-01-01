583243c28c1480a7f8edf3fc3683b177d39e0ca2
Terry Weymouth <weymouth@umich.edu>
2024-01-01 14:39:15 -0500

    transfer Desktop to Laptop

--- exploration/point_on_road.py
+++ exploration/point_on_road.py
@@ -19,16 +19,19 @@ def make_connection():
 def compute_point_on_road(track_id, osm_id):
     # ST_Distance(ST_Transform(osm.way,4326),track.long_lat_original)
     # ST_AsText(ST_ClosestPoint(track.long_lat_original,ST_Transform(osm.way,4326))) as cp
-    query_str = """
+    """
     select  ST_AsText(ST_ClosestPoint(line, point)) as cp1,
         ST_AsText(ST_ClosestPoint(point, line)) as cp2,
         ST_AsText(point),
         ST_AsText(ST_ShortestLine(point, line)) as line_between1,
         ST_AsText(ST_ShortestLine(line, point)) as line_between2
-	from bicycle_data as track, planet_osm_line as osm,
-    	LATERAL (select ST_Transform(osm.way,4326) as line from planet_osm_line where osm_id=8699583) as a,
-    	LATERAL (select long_lat_original as point from bicycle_data where id=27) as b
+    from bicycle_data as track, planet_osm_line as osm,
+        LATERAL (select ST_Transform(osm.way,4326) as line from planet_osm_line where osm_id=8699583) as a,
+        LATERAL (select long_lat_original as point from bicycle_data where id=27) as b
     where track.id=27 and osm.osm_id=8699583
+    """
+    query_str = """
+    
     """
     query = sql.SQL(query_str)
     cursor.execute(query)
@@ -40,8 +43,9 @@ def main():
     global connection, cursor
     make_connection()
     # value = compute_point_on_road(1402, 8699583)
-    value = compute_point_on_road(27, 8699583)
-    print(f"{value}")
+    value_tuple = compute_point_on_road(27, 8699583)
+    for i in range(0, len(value_tuple)):
+        print(f"{value_tuple[i]}")
     if cursor:
         cursor.close()
     if connection:
